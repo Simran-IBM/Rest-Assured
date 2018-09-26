b447b8d9b1f646e4676675fec910fd845d344760
SimranRai22 <simran.pre22@gmail.com>
2018-09-27 00:08:13 +0530

    Initial commit for Rest-Assured

---
+++ src/test/java/com/test/restAssured/restAssured/GetData.java
@@ -0,0 +1,32 @@
+package com.test.restAssured.restAssured;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+
+public class GetData {
+	
+	@Test(priority=1)
+	public void testResponseCode() {
+		Response resp=RestAssured.get("https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=b1b15e88fa797225412429c1c50c122a1");
+		int code=resp.getStatusCode();
+		int expectedcode=200;
+		System.out.println("Status code is "+code);
+		
+		Assert.assertEquals(code,expectedcode);
+		
+	}
+	
+	@Test(priority=2)
+	public void testResponseBody() {
+		Response resp=RestAssured.get("https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=b1b15e88fa797225412429c1c50c122a1");
+		String data=resp.asString();
+		System.out.println("Data is "+data);
+		
+		System.out.println("Response Time: "+resp.getTime());
+		
+	}
+
+}
